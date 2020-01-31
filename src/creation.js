b5c3fd3bcee73beac8578d2036cb817d935cb8cc
ekater1na <ekaterina_l@tut.by>
2020-01-31 18:11:10 +0300

    feat: create stream

--- src/creation.js
+++ src/creation.js
@@ -1,16 +1,31 @@
-import { of, from } from 'rxjs';
+import { of, from, Observable, observable } from 'rxjs';
 import { scan } from 'rxjs/operators';
 
-const stream$ = of(1, 2, "Rrrrr", 4, 5, 6, 7);
+// const stream$ = of(1, 2, "Rrrrr", 4, 5, 6, 7);
 
-stream$.subscribe(val => {
-    console.log("Value: ", val);
+// stream$.subscribe(val => {
+//     console.log("Value: ", val);
+// })
+
+// const array$ = from([1, 2, 3, 4]).pipe(
+//    scan( (acc, v) => acc.concat(v), [])
+// );
+
+// array$.subscribe(val => {
+//     console.log("Value: ", val);
+// })
+
+const stream$ = new Observable(observer => {
+    observer.next('First value')
+    setTimeout( () => observer.next('After 1000 ms'), 1000)
+    setTimeout( () => observer.complete(), 1500)
+    setTimeout( () => observer.error('Error'), 2000)
+    setTimeout( () => observer.next('After 3000 ms'), 3000)
 })
 
-const array$ = from([1, 2, 3, 4]).pipe(
-   scan( (acc, v) => acc.concat(v), [])
-);
-
-array$.subscribe(val => {
-    console.log("Value: ", val);
-})
+stream$.subscribe( 
+    (val) => console.log("Val: ", val),
+    (err) => console.log(err),
+    () => console.log("Complete")
+    
+    )
